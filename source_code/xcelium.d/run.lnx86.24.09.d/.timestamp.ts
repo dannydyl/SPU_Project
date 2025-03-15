b38b51e666f910d580e1a88db3b0dd82517c6352
Dongyun Lee <dannydyl8@gmail.com>
2025-03-14 21:53:49 -0400

    xrun or xmvlog complie

--- source_code/xcelium.d/run.lnx86.24.09.d/.timestamp.ts
+++ source_code/xcelium.d/run.lnx86.24.09.d/.timestamp.ts
@@ -1,4 +1,4 @@
 1741877625 /home/home5/dlee/ese545work/SPU_Project/source_code/tb_RF_FU_Pipe_wrapper.v
 1741364540 /home/home5/dlee/ese545work/SPU_Project/source_code/tb_Even_Pipe.v
 1741362814 /home/home5/dlee/ese545work/SPU_Project/source_code/Even_Pipe.v
-1741876543 /home/home5/dlee/ese545work/SPU_Project/source_code/RF_FU_Pipe_wrapper.v
+1741879923 /home/home5/dlee/ese545work/SPU_Project/source_code/RF_FU_Pipe_wrapper.v
--- source_code/xcelium.d/run.lnx86.24.09.d/files.ts
+++ source_code/xcelium.d/run.lnx86.24.09.d/files.ts
@@ -1,4 +1,4 @@
 1741364540 /home/home5/dlee/ese545work/SPU_Project/source_code/tb_Even_Pipe.v
-1741876543 /home/home5/dlee/ese545work/SPU_Project/source_code/Even_Pipe.v
-1741877625 /home/home5/dlee/ese545work/SPU_Project/source_code/tb_RF_FU_Pipe_wrapper.v
-1741876543 /home/home5/dlee/ese545work/SPU_Project/source_code/RF_FU_Pipe_wrapper.v
+1741897168 /home/home5/dlee/ese545work/SPU_Project/source_code/Even_Pipe.v
+1741879917 /home/home5/dlee/ese545work/SPU_Project/source_code/tb_RF_FU_Pipe_wrapper.v
+1741879923 /home/home5/dlee/ese545work/SPU_Project/source_code/RF_FU_Pipe_wrapper.v
