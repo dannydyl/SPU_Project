2edf22c4f9d5ecf3ec8d4d882d45cd449dea615a
Dongyun Lee <dannydyl8@gmail.com>
2025-04-06 16:07:26 -0400

    pipeline well

--- source_code/RF_FU_Pipe_wrapper.v
+++ source_code/RF_FU_Pipe_wrapper.v
@@ -66,40 +66,114 @@ wire WB_reg_write_en_even, WB_reg_write_en_odd;
 
 wire [0:9] current_PC_odd, new_PC_odd;
 
+
+// register placed before RF_FU_wrapper module
+reg [0:31] full_instr_even_reg, full_instr_odd_reg;
+reg [0:6] instr_id_even_reg, instr_id_odd_reg;
+reg [0:6] reg_dst_even_reg, reg_dst_odd_reg;
+reg [0:2] unit_id_even_reg, unit_id_odd_reg;
+reg [0:3] latency_even_reg, latency_odd_reg;
+reg reg_wr_even_reg, reg_wr_odd_reg;
+reg [0:6] imme7_even_reg, imme7_odd_reg;
+reg [0:9] imme10_even_reg, imme10_odd_reg;
+reg [0:15] imme16_even_reg, imme16_odd_reg;
+reg [0:17] imme18_even_reg, imme18_odd_reg;
+reg [0:6] ra_addr_even_reg, ra_addr_odd_reg,
+          rb_addr_even_reg, rb_addr_odd_reg,
+          rc_addr_even_reg, rc_addr_odd_reg;
+
+always @(posedge clk or posedge rst) begin
+  if (rst) begin
+    full_instr_even_reg <= 0;
+    full_instr_odd_reg <= 0;
+    instr_id_even_reg <= 0;
+    instr_id_odd_reg <= 0;
+    reg_dst_even_reg <= 0;
+    reg_dst_odd_reg <= 0;
+    unit_id_even_reg <= 0;
+    unit_id_odd_reg <= 0;
+    latency_even_reg <= 0;
+    latency_odd_reg <= 0;
+    reg_wr_even_reg <= 0;
+    reg_wr_odd_reg <= 0;
+    imme7_even_reg <= 0;
+    imme7_odd_reg <= 0;
+    imme10_even_reg <= 0;
+    imme10_odd_reg <= 0;
+    imme16_even_reg <= 0;
+    imme16_odd_reg <= 0;
+    imme18_even_reg <= 0;
+    imme18_odd_reg <= 0;
+    ra_addr_even_reg <= 0;
+    ra_addr_odd_reg <= 0;
+    rb_addr_even_reg <= 0;
+    rb_addr_odd_reg <= 0;
+    rc_addr_even_reg <= 0;
+    rc_addr_odd_reg <= 0;
+  end else begin
+    full_instr_even_reg <= full_instr_even;
+    full_instr_odd_reg <= full_instr_odd;
+    instr_id_even_reg <= instr_id_even;
+    instr_id_odd_reg <= instr_id_odd;
+    reg_dst_even_reg <= reg_dst_even;
+    reg_dst_odd_reg <= reg_dst_odd;
+    unit_id_even_reg <= unit_id_even;
+    unit_id_odd_reg <= unit_id_odd;
+    latency_even_reg <= latency_even;
+    latency_odd_reg <= latency_odd;
+    reg_wr_even_reg <= reg_wr_even;
+    reg_wr_odd_reg <= reg_wr_odd;
+    imme7_even_reg <= imme7_even;
+    imme7_odd_reg <= imme7_odd;
+    imme10_even_reg <= imme10_even;
+    imme10_odd_reg <= imme10_odd;
+    imme16_even_reg <= imme16_even;
+    imme16_odd_reg <= imme16_odd;
+    imme18_even_reg <= imme18_even;
+    imme18_odd_reg <= imme18_odd;
+    ra_addr_even_reg <= ra_addr_even;
+    ra_addr_odd_reg <= ra_addr_odd;
+    rb_addr_even_reg <= rb_addr_even;
+    rb_addr_odd_reg <= rb_addr_odd;
+    rc_addr_even_reg <= rc_addr_even;
+    rc_addr_odd_reg <= rc_addr_odd;
+  end
+end
+
 RF_FU_wrapper RF_FU_wrapper_inst (
   .clk(clk),
   .rst(rst),
 
   // from ID 
-  .full_instr_even(full_instr_even),
-  .instr_id_even(instr_id_even),
-  .reg_dst_even(reg_dst_even),
-  .unit_id_even(unit_id_even),
-  .latency_even(latency_even),
-  .reg_wr_even(reg_wr_even),
-  .imme7_even(imme7_even),  // could be reduced to two signals since I7, I10 last bit index is same
-  .imme10_even(imme10_even), // and I16, I18 last bit index is same
-  .imme16_even(imme16_even),  // or could be extracted from full instr based on which instruction
-  .imme18_even(imme18_even),
+  .full_instr_even(full_instr_even_reg),
+  .instr_id_even(instr_id_even_reg),
+  .reg_dst_even(reg_dst_even_reg),
+  .unit_id_even(unit_id_even_reg),
+  .latency_even(latency_even_reg),
+  .reg_wr_even(reg_wr_even_reg),
+  .imme7_even(imme7_even_reg),  // could be reduced to two signals since I7, I10 last bit index is same
+  .imme10_even(imme10_even_reg), // and I16, I18 last bit index is same
+  .imme16_even(imme16_even_reg),  // or could be extracted from full instr based on which instruction
+  .imme18_even(imme18_even_reg),
 
-  .full_instr_odd(full_instr_odd),
-  .instr_id_odd(instr_id_odd),
-  .reg_dst_odd(reg_dst_odd),
-  .unit_id_odd(unit_id_odd),
-  .latency_odd(latency_odd),
-  .reg_wr_odd(reg_wr_odd),
-  .imme7_odd(imme7_odd), 
-  .imme10_odd(imme10_odd),
-  .imme16_odd(imme16_odd),
-  .imme18_odd(imme18_odd),
+  .full_instr_odd(full_instr_odd_reg),
+  .instr_id_odd(instr_id_odd_reg),
+  .reg_dst_odd(reg_dst_odd_reg),
+  .unit_id_odd(unit_id_odd_reg),
+  .latency_odd(latency_odd_reg),
+  .reg_wr_odd(reg_wr_odd_reg),
+  .imme7_odd(imme7_odd_reg), 
+  .imme10_odd(imme10_odd_reg),
+  .imme16_odd(imme16_odd_reg),
+  .imme18_odd(imme18_odd_reg),
 
   // register file access
-  .ra_addr_even(ra_addr_even),
-  .rb_addr_even(rb_addr_even),
-  .rc_addr_even(rc_addr_even),
-  .ra_addr_odd(ra_addr_odd),
-  .rb_addr_odd(rb_addr_odd),
-  .rc_addr_odd(rc_addr_odd),
+  .ra_addr_even(ra_addr_even_reg),
+  .rb_addr_even(rb_addr_even_reg),
+  .rc_addr_even(rc_addr_even_reg),
+  .ra_addr_odd(ra_addr_odd_reg),
+  .rb_addr_odd(rb_addr_odd_reg),
+  .rc_addr_odd(rc_addr_odd_reg),
   .reg_write_en_1(WB_reg_write_en_even),
   .reg_write_en_2(WB_reg_write_en_odd),
   .reg_write_addr_1(WB_reg_write_addr_even),
@@ -224,15 +298,5 @@ Program_Counter Program_Counter_inst( // only for branch verification purpose. S
   .PC_out(new_PC_odd)
 );
 
-// always @(posedge clk or posedge rst) begin
-//   if (rst) begin
-//     packed_1stage_even <= 0;
-//     packed_1stage_odd <= 0;
-//   end
-//   else begin
-//     packed_1stage_even <= packed_2stage_even;
-//     packed_1stage_odd <= packed_2stage_odd;
-//   end
-// end
 endmodule
 
